05d59cf795536de7770d82f1c7694117f06e3d04
Nataliia Oliinyk <nataliia.oliinyk.de@gmail.com>
2025-10-16 15:31:41 +0200

    update logo

--- src/shared/Logo/Logo.tsx
+++ src/shared/Logo/Logo.tsx
@@ -1,16 +1,14 @@
 import type { FC } from "react";
 import { Link } from "react-router-dom";
 
-import logo from "../../../public/logo/logo.png";
+import logo from "/logo/logo.png";
 
 import styles from "./Logo.module.css";
-
-
 
 const Logo: FC = () => {
   return (
     <Link to="/">
-      <img src={logo} alt="Logo" className={styles.logo}/>
+      <img src={logo} alt="Logo" className={styles.logo} />
     </Link>
   );
 };
