8786837ea21c259d5e9a0696c179b86e7520ed06
Nataliia Oliinyk <nataliia.oliinyk.de@gmail.com>
2025-11-18 18:22:32 +0100

    add about page

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -1,13 +1,23 @@
 import type { FC } from "react";
+import { useTranslation } from "react-i18next";
+
+import Wrapper from "../../shared/components/Wrapper/Wrapper";
+import SectionLayout from "../../shared/components/SectionLayout/SectionLayout";
 
 import styles from "./AboutPage.module.css";
 
 const AboutPage: FC = ()=>{
-    return (
-        <div>
-            <h3>AboutPage</h3>
-        </div>
-    )
+    const { t } = useTranslation(["about"]);
+
+  return (
+    <main>
+      <Wrapper>
+        <SectionLayout title={t("about:hero.title")}>
+          <div>AboutPage</div>
+        </SectionLayout>
+      </Wrapper>
+    </main>
+  );
 };
 
 export default AboutPage;
