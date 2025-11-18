d6e87397cf9c85dcbe50f887cfb16ee2e39b457c
Nataliia Oliinyk <nataliia.oliinyk.de@gmail.com>
2025-11-18 18:22:17 +0100

    update projects text

--- src/modules/MyProjects/MyProjects.tsx
+++ src/modules/MyProjects/MyProjects.tsx
@@ -3,18 +3,19 @@ import { useTranslation } from "react-i18next";
 
 import Container from "../../shared/components/Container/Container";
 import Subtitle from "../../shared/components/Subtitle/Subtitle";
+import ButtonLinks from "../../shared/components/ButtonLinks/ButtonLinks";
 
-// import styles from "./MyProjects.module.css";
+import styles from "./MyProjects.module.css";
 
 const MyProjects: FC = () => {
-
-    const { t } = useTranslation(["projects"]);
-
+  const { t } = useTranslation(["projects"]);
 
   return (
-    <Container >
-      <Subtitle text={t("projects:hero.subtitle")} />
-      
+    <Container>
+      <div className={styles.box}>
+        <Subtitle text={t("projects:hero.subtitle")} />
+        <ButtonLinks href="/contact" text={t("projects:hero.cta")} className={styles.contactLink}/>
+      </div>
     </Container>
   );
 };
