652df6e19cca430f0cb2edf21520f17655dd6b9b
Nataliia Oliinyk <nataliia.oliinyk.de@gmail.com>
2025-11-19 23:41:02 +0100

    add Not Found page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,9 +1,40 @@
 import type { FC } from "react";
+import { useTranslation } from "react-i18next";
 
-// import styles from "./NotFoundPage.module.css";
+import Wrapper from "../../shared/components/Wrapper/Wrapper";
+import SectionLayout from "../../shared/components/SectionLayout/SectionLayout";
+import Container from "../../shared/components/Container/Container";
+import Subtitle from "../../shared/components/Subtitle/Subtitle";
+import ButtonLinks from "../../shared/components/ButtonLinks/ButtonLinks";
+
+import styles from "./NotFoundPage.module.css";
 
 const NotFoundPage: FC = () => {
-  return <div>NotFoundPage</div>;
+  const { t } = useTranslation(["notFound"]);
+
+  return (
+    <main>
+      <Wrapper>
+        <SectionLayout title={t("notFound:title")}>
+          <Container>
+            <Subtitle text={t("notFound:subtitle")} />
+            <div className={styles.btnBox}>
+              <ButtonLinks
+                href="/"
+                text={t("notFound:cta1")}
+                className={styles.contactLink}
+              />
+              <ButtonLinks
+                href="/projects"
+                text={t("notFound:cta2")}
+                className={styles.contactLink}
+              />
+            </div>
+          </Container>
+        </SectionLayout>
+      </Wrapper>
+    </main>
+  );
 };
 
 export default NotFoundPage;
