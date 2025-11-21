14f353e648b5f773e65a399536e905cb6545cbff
Nataliia Oliinyk <nataliia.oliinyk.de@gmail.com>
2025-11-21 22:30:48 +0100

    update

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from "react-i18next";
 import Wrapper from "../../shared/components/Wrapper/Wrapper";
 import SectionLayout from "../../shared/components/SectionLayout/SectionLayout";
 
-import styles from "./AboutPage.module.css";
+// import styles from "./AboutPage.module.css";
 
 const AboutPage: FC = ()=>{
     const { t } = useTranslation(["about"]);
