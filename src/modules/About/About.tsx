34a053fa875d1fd1b1ff0604afc73ff764d2f91f
Nataliia Oliinyk <nataliia.oliinyk.de@gmail.com>
2025-11-23 14:23:38 +0100

    add About page

---
+++ src/modules/About/About.tsx
@@ -0,0 +1,15 @@
+import type { FC } from "react";
+
+import Container from "../../shared/components/Container/Container";
+
+// import styles from "./About.module.css";
+
+const About: FC = () => {
+  return (
+    <Container>
+      <p>About</p>
+    </Container>
+  );
+};
+
+export default About;
